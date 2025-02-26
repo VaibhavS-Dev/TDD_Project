09a2c7608e1827db2463f2df67eca8eae382b8da
Vaibhav <vaibhav79835@gmail.com>
2025-02-26 17:51:26 +0530

    Added support for new lines, custom delimiters, and negative number validation

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -1,7 +1,8 @@
 const add = require("./stringCalculator");
 
-test("supports custom delimiters", () => {
-    expect(add("//;\n1;2")).toBe(3); // 1 + 2 = 3
+test("throws an error for negative numbers", () => {
+    expect(() => add("1,-2,3,-4")).toThrow("negative numbers not allowed -2, -4");
   });
+  
   
   
